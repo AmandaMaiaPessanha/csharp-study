5efac86fde2a47fe8ec55cc2582beebbe50a728a
Amanda Maia <amandampessanha@hotmail.com>
2021-01-11 00:37:40 -0300

    :sparkles: Implement params.

--- csharpStudy/csharpStudy/Program.cs
+++ csharpStudy/csharpStudy/Program.cs
@@ -47,7 +47,8 @@ namespace csharpStudy
                 {"Métodos com retorno - Classes e Métodos", MetodosComRetorno.Executar },
                 {"Métodos estaticos - Classes e Métodos", MetodosEstaticos.Executar },
                 {"Atributos estaticos - Classes e Métodos", AtributosEstaticos.Executar },
-                {"Desafio Atributo - Classes e Métodos", DesafioAtributo.Executar }
+                {"Desafio Atributo - Classes e Métodos", DesafioAtributo.Executar },
+                {"Params - Classes e Métodos", Params.Executar }
 
             });
 
